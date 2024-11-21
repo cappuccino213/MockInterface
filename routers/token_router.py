84094ef7742a7b990228103f74fae6a0a1c46dab
zhangyp <541159401@qq.com>
2024-11-21 09:34:08 +0800

    token_router增加获取真实token方法，token服务地址在配置文件可配

--- routers/token_router.py
+++ routers/token_router.py
@@ -5,9 +5,12 @@
 @Contact : yeahcheung213@163.com
 """
 import jwt
-from fastapi import APIRouter
-from fastapi.responses import RedirectResponse
 
+import requests
+
+from fastapi import APIRouter
+
+from config import CONFIG
 from request_model import RequestToken, RequestOld
 from response_model import Token, ValidateToken, CheckStatus, RegisterInfo
 
@@ -26,6 +29,9 @@ product_route = APIRouter(tags=['Token'])
 
 SECRET = "eWordTomTaw@HZ"
 
+# 获取参数
+config = CONFIG
+
 
 def jwt_token(token_dict, secret=SECRET, algorithms="HS256"):
     """
@@ -38,6 +44,23 @@ def jwt_token(token_dict, secret=SECRET, algorithms="HS256"):
     return token_value
 
 
+# 尝试获取真实的token，若返回401错误，则返回{}，否则返回真实信息
+def get_real_token(fetch_api: str, request_params: dict):
+    real_token_host = config['proxy']['token_host']
+    real_token_url = f"{real_token_host}{fetch_api}"
+    try:
+        response = requests.post(real_token_url, json=request_params)
+        if response:
+            if response.json()['status'] == 0:
+                print(f"INFO：产品信息{request_params}已注册，返回真实的token信息")
+                return response.json()
+        print(f"WARNING:{response.text}")
+        return {}
+    except Exception as e:
+        print(f"ERROR:{e}")
+        return {}
+
+
 # 校验是否注册
 @product_route.get('/Product/CheckProductRegisterStateByHospital', name="校验产品注册情况", response_model=CheckStatus)
 def check_product_register(hospitalCode: str, productName: str):
@@ -47,8 +70,13 @@ def check_product_register(hospitalCode: str, productName: str):
     return CheckStatus(**check_result)
 
 
-@token_route.post('/RetriveInternal', name="获取令牌", response_model=Token)
+@token_route.post('/RetriveInternal', name="获取令牌", response_model=Token,
+                  description="优先返回真实token信息，若未注册则返回mock的token")
 def retrieve_internal_token(request_token: RequestToken):
+    # 先尝试获取真实的token
+    real_token = get_real_token("/Token/RetriveInternal", request_token.model_dump())
+    if real_token:
+        return Token(**real_token)
     token_body = dict(token=jwt_token(request_token.model_dump()), desc="success", status=0)
     return Token(**token_body)
 
@@ -60,13 +88,18 @@ def validate_internal_token(request_token: RequestToken):
 
 
 # 兼容老版本token接口
-@token_route.post('/Retrive', name="获取令牌（老版本）", response_model=Token)
+@token_route.post('/Retrive', name="获取令牌（老版本）", response_model=Token,
+                  description="优先返回真实token信息，若未注册则返回mock的token")
 def retrieve_token(request_token: RequestOld):
+    real_token = get_real_token("/Token/Retrive", request_token.model_dump())
+    if real_token:
+        return Token(**real_token)
     token_body = dict(token=jwt_token(request_token.model_dump()), desc="success", status=0)
     return Token(**token_body)
 
 
-@token_route.post('/Validate', name="验证令牌(老版本)", response_model=ValidateToken)
+@token_route.post('/Validate', name="验证令牌(老版本)", response_model=ValidateToken,
+                  description="优先返回真实token信息，若未注册则返回mock的token")
 def validate_token():
     validate_result = dict(desc="成功", status=0)
     return ValidateToken(**validate_result)
@@ -75,6 +108,9 @@ def validate_token():
 # 产品之间验证
 @token_route.post('/RetriveInteractive', name="产品间获取令牌", response_model=Token)
 def retrieve_interactive_token(request_token: RequestToken):
+    real_token = get_real_token("/Token/RetriveInteractive", request_token.model_dump())
+    if real_token:
+        return Token(**real_token)
     token_body = dict(token=jwt_token(request_token.model_dump()), desc="success", status=0)
     return Token(**token_body)
 
